f3eab7fa66d1860bea3b284f27044c8833b1c61c
Drew Banyai <DrewBanyai@gmail.com>
2019-06-16 19:18:51 -0700

    - Updating to an external IP to test the microservice

--- Site/webAPI.js
+++ Site/webAPI.js
@@ -1,3 +1,5 @@
+let serviceHost = "98.181.188.165";
+
 let makeRequest = async (options) => {
     let fetchOptions = {
         method: options.method,
@@ -10,7 +12,7 @@ let makeRequest = async (options) => {
         body: options.hasOwnProperty("body") ? options.body : null,
     };
 
-    let response = await fetch("http://localhost:3000/" + options.endpoint, fetchOptions);
+    let response = await fetch(`${serviceHost}:3000/` + options.endpoint, fetchOptions);
     return response;
 };
 
